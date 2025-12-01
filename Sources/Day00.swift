c864ed3b3b17311983510acdc2db6a337b0b7776
Shawky Elhanak <shawky@elhanak.com>
2025-12-01 15:29:51 +0100

    Correct answer for day 01 part 1

--- Sources/Day00.swift
+++ Sources/Day00.swift
@@ -1,24 +1,33 @@
 import Algorithms
 
 struct Day00: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
+
   var data: String
 
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
-    }
+  var entities: [String] {
+    data.split(separator: "\n").map { String($0) }
   }
 
-  // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    3
+    var current = 50
+    var count = 0
+    for entity in entities {
+      let turn = entity.first!
+      let amount = Int(entity.dropFirst())!
+      current = turn == "L" ? current - amount : current + amount
+      while current < 0 {
+        current += 100
+      }
+      while current >= 100 {
+        current -= 100
+      }
+      count = current == 0 ? count + 1 : count
+    }
+    return count
   }
 
-  // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    // Currently unimplemented; remove when you have a solution.
+    0
   }
 }
