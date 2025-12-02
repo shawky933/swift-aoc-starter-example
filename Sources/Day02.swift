ca08d7622b278a1d26f7649d8b2158b795bc93c9
Shawky Elhanak <shawky@elhanak.com>
2025-12-02 09:27:22 +0100

    Correct answer for day 02 part 1

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -17,7 +17,18 @@ struct Day02: AdventDay {
   }
 
   func part1() -> Any {
-    1227775554
+    var sum = 0
+    for range in entities {
+      for i in range {
+        let str = String(describing: i)
+        let midIndex = str.index(str.startIndex, offsetBy: str.count / 2)
+        let firstHalf = str[..<midIndex]
+        let secondHalf = str[midIndex...]
+        guard firstHalf == secondHalf else { continue }
+        sum += i
+      }
+    }
+    return sum
   }
 
   func part2() -> Any {
