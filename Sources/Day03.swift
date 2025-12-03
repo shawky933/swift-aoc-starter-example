1a0cf5e5ca029dfb65e0529935ed072fb6a3b0d7
Shawky Elhanak <shawky@elhanak.com>
2025-12-03 15:32:23 +0100

    Correct answer for day 03 part 1

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -16,7 +16,25 @@ struct Day03: AdventDay {
   }
 
   func part1() -> Any {
-    357
+    var sum = 0
+    for bank in entities {
+      let maxBattery = bank.max()!
+      let maxIndex = bank.firstIndex(of: maxBattery)!
+      let leftSplice = bank[0..<maxIndex]
+      let rightSplice = bank[(maxIndex + 1)..<bank.count]
+      let leftMax = leftSplice.max()
+      let rightMax = rightSplice.max()
+      var leftNumber: Int?
+      var rightNumber: Int?
+      if let leftMax {
+        leftNumber = leftMax * 10 + maxBattery
+      }
+      if let rightMax {
+        rightNumber = maxBattery * 10 + rightMax
+      }
+      sum += max(leftNumber ?? 0, rightNumber ?? 0)
+    }
+    return sum
   }
 
   func part2() -> Any {
