0394797e1a9a44c6b2b30a75098973e21436c703
Shawky Elhanak <shawky@elhanak.com>
2025-12-04 10:27:09 +0100

    Correct answer for Day 04 part 2

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -9,20 +9,16 @@ struct Day04: AdventDay {
 
   var data: String
 
-  var entities: [[String]] {
-    data.split(separator: "\n").map { $0.map { String($0) }  }
-  }
-
-  let _entities: [[String]]
+  private var entities: [[String]]
 
   init(data: String) {
     self.data = data
-    self._entities = data.split(separator: "\n").map { $0.map { String($0) }  }
+    self.entities = data.split(separator: "\n").map { $0.map { String($0) }  }
   }
 
   func part1() -> Any {
     var count = 0
-    for (i, row) in _entities.enumerated() {
+    for (i, row) in entities.enumerated() {
       for (j, position) in row.enumerated() {
         guard position == "@" else { continue }
         var adjacentCount = 0
@@ -30,7 +26,7 @@ struct Day04: AdventDay {
         for (dx, dy) in directions {
           let newX = i + dx
           let newY = j + dy
-          guard newX >= 0 && newX < _entities.count && newY >= 0 && newY < row.count else { continue }
+          guard newX >= 0 && newX < entities.count && newY >= 0 && newY < row.count else { continue }
           if entities[newX][newY] == "@" {
             adjacentCount += 1
           }
@@ -44,6 +40,31 @@ struct Day04: AdventDay {
   }
 
   func part2() -> Any {
-    43
+    var count = 0
+    var currentEntities = entities
+    var currentCount = -1
+    while currentCount != count {
+      currentCount = count
+      for (i, row) in currentEntities.enumerated() {
+        for (j, position) in row.enumerated() {
+          guard position == "@" else { continue }
+          var adjacentCount = 0
+          let directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+          for (dx, dy) in directions {
+            let newX = i + dx
+            let newY = j + dy
+            guard newX >= 0 && newX < currentEntities.count && newY >= 0 && newY < row.count else { continue }
+            if currentEntities[newX][newY] == "@" {
+              adjacentCount += 1
+            }
+          }
+          if adjacentCount < 4 {
+            count += 1
+            currentEntities[i][j] = "."
+          }
+        }
+      }
+    }
+    return count
   }
 }
